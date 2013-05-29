06f0432b94469b6b4befaa981e658cfcfa720731
RaskolnikovRR <s.rajagopal@iitg.ernet.in>
2013-05-29 18:32:39 +0530

    This is now the base function and everything else called from here.

--- optimiser.m
+++ optimiser.m
@@ -1,12 +1,34 @@
-function [points,results] = optimiser()
+function [x,fval] = optimiser()
 
+%% Ps,Pyx definition
+% Channel property Pxy and source Ps are defined
+p = 0.6;
+Ps = [0.5 0.5];
+Pxy = [p 1-p;1-p p];
+
+%% function definition and intervals
+f = @(x,y,s,h)( (s-h)^2 + x^2);
+[xmin,ymin,hmin,smin] = deal(0);
+[xmax,smax,hmax,ymax] = deal(2);
+[xcuts,ycuts,scuts,hcuts] = deal(2);
+
+%% Call to intbymat4.m
+% to compute dels,number of elements (xi,yi,si,hi)
+[xdel,ydel,sdel,hdel,xi,yi,si,hi,iRes,F] = intbymat4(f,xmin,xmax,ymin,ymax,smin,smax,hmin,hmax,xcuts,ycuts,scuts,hcuts);
+
+%% Call to convexopt.m
+% to compute Aeq,beq,A,b
+[intmatC1,intmatC2] = convexopt(xdel,ydel,sdel,hdel,xi,yi,si,hi,F);
+
+%% definition of dels, ni
+dels = [xdel ydel sdel hdel];
+ni = [xi yi si hi];
+
+%% Call to fmincon.m
+Aeq = cat(1,intmatC1,intmatC2);
+beq = cat(1,transpose(Ps),[1]);
 options = optimset('Algorithm','interior-point');
+[lb,x0] = deal(zeros(xi*yi*si*hi,1));
+[x fval] = fmincon(@(Q) costfun(Q),x0,[],[],Aeq,beq,lb,[],@(Q) nonlcon(Ps,Pxy,Q,dels,ni) ,options);
 
-p = 0.1;
-i = 1;
-ilims = 10;
-while (i < ilims && p < 1)
-    [points(i,:) results(i)] = fmincon(@(Q) costfun(Q),zeros(1,16),[],[],[],[],zeros(1,16),[],@(Q) nonlcon(p,Q),options);
-    p = p + 1/ilims;
-    i = i + 1;
-end
+end % main
