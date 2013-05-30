4d4095881c53130d4f917909dc676ceab2298b3e
RaskolnikovRR <s.rajagopal@iitg.ernet.in>
2013-05-30 15:53:54 +0530

    FIX: changed output vars of convexopt.m to include intmatC3, change in Aeq,beq

--- optimiser.m
+++ optimiser.m
@@ -1,5 +1,4 @@
 function [x,fval] = optimiser()
-
 %% Ps,Pyx definition
 % Channel property Pxy and source Ps are defined
 p = 0.6;
@@ -18,15 +17,15 @@ f = @(x,y,s,h)( (s-h)^2 + x^2);
 
 %% Call to convexopt.m
 % to compute Aeq,beq,A,b
-[intmatC1,intmatC2] = convexopt(xdel,ydel,sdel,hdel,xi,yi,si,hi,F);
+[intmatC1,intmatC2,intmatC3] = convexopt(xdel,ydel,sdel,hdel,xi,yi,si,hi,transpose(F),Pxy);
 
 %% definition of dels, ni
 dels = [xdel ydel sdel hdel];
 ni = [xi yi si hi];
 
 %% Call to fmincon.m
-Aeq = cat(1,intmatC1,intmatC2);
-beq = cat(1,transpose(Ps),[1]);
+Aeq = cat(1,intmatC1,intmatC2,intmatC3);
+beq = cat(1,transpose(Ps),[1],zeros(xi*yi,1));
 options = optimset('Algorithm','interior-point');
 [lb,x0] = deal(zeros(xi*yi*si*hi,1));
 [x fval] = fmincon(@(Q) costfun(Q),x0,[],[],Aeq,beq,lb,[],@(Q) nonlcon(Ps,Pxy,Q,dels,ni) ,options);
