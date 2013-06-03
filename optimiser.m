dbe0b8d22b642f49deb5c5b5dd7ba76d131210b5
RaskolnikovRR <s.rajagopal@iitg.ernet.in>
2013-06-03 16:48:18 +0530

    FIX: generalised code for any length of s,x,y,h

--- optimiser.m
+++ optimiser.m
@@ -2,24 +2,39 @@ function [xA,fvalA] = optimiser()
 %% function definition and intervals
 f = @(x,y,s,h)( (s-h)^2 + x^2);
 [xmin,ymin,hmin,smin] = deal(0);
-[xmax,smax,hmax,ymax] = deal(2);
-[xcuts,ycuts,scuts,hcuts] = deal(2);
+[xmax,smax,hmax,ymax] = deal(12);
+[xcuts,ycuts,scuts,hcuts] = deal(12);
 
 %% del calculation
-xdel = (xmax - xmin)/2;
-ydel = (ymax - ymin)/2;
-sdel = (smax - smin)/2;
-hdel = (hmax - hmin)/2;
+xdel = (xmax - xmin)/xcuts;
+ydel = (ymax - ymin)/ycuts;
+sdel = (smax - smin)/scuts;
+hdel = (hmax - hmin)/hcuts;
 
 %% Ps,Pyx definition
 % Channel property Pxy and source Ps are defined
-xA = [];
-fvalA = [];
-p = 0.7;
-% Ps = [smin:sdel:smax];
-% Ps = exp(-Ps.^2/2/sigma^2)/sigma/sqrt(2*pi);
-Ps = [0.5 0.5];
-Pxy = [p 1-p;1-p p];
+sig = 1;
+Ps = [smin + sdel:sdel:smax];
+Ps = exp(-Ps.^2/2/sig^2)/sig/sqrt(2*pi);
+
+Pxy = zeros(xcuts,ycuts);
+curX = xmin;
+curY = ymin;
+
+ii = 1;
+check = 0;
+while ii <= ycuts
+    jj = 1;
+    curX = xmin;
+    while jj <= xcuts
+        Pxy(jj,ii) = vpa(exp( -(curY - curX)^2/2)/sqrt(2*pi),32);
+        curX = curX + xdel;
+        jj = jj + 1;
+        check = check + 1;
+    end
+    curY = curY + ydel;
+    ii = ii + 1;
+end
 
 %% Call to intbymat4.m
 % to compute dels,number of elements (xi,yi,si,hi)
@@ -27,7 +42,7 @@ Pxy = [p 1-p;1-p p];
 
 %% Call to convexopt.m
 % to compute Aeq,beq,A,b
-[intmats,intmatC1,intmatC2,intmatC3] = convexopt(1,1,1,1,2,2,2,2,Pxy);
+[intmats,intmatC1,intmatC2,intmatC3] = convexopt(xdel,ydel,sdel,hdel,xi,yi,si,hi,Pxy);
 
 %% definition of dels, ni
 dels = [xdel ydel sdel hdel];
