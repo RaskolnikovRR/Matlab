5aaab01c1c69b68a7b0709a223d5ca4007020bf0
RaskolnikovRR <s.rajagopal@iitg.ernet.in>
2013-06-05 17:45:42 +0530

    FIX: Pxy extended on both sides to include 3*sigma limits for all rows.

--- optimiser.m
+++ optimiser.m
@@ -1,8 +1,9 @@
 function [x,fval] = optimiser()
 %% function definition and intervals
+tic;
 [xmin,ymin,hmin,smin] = deal(-6);
 [xmax,smax,hmax,ymax] = deal(6);
-[xcuts,ycuts,scuts,hcuts] = deal(5);
+[xcuts,ycuts,scuts,hcuts] = deal(3);
 
 cutscell = num2cell([xcuts ycuts scuts hcuts]);
 [xi yi si hi] = cutscell{:};
@@ -24,14 +25,14 @@ iPs = [smin + sdel:sdel:smax];
 Ps = cdf('norm',iPs,0,1) - [0 cdf('norm',[iPs(2:length(iPs))-sdel],0,1)];
 
 iPx = [xmin + xdel:xdel:xmax]';
-iPy = [ymin + ydel:ydel:ymax];
-Pxy1 = cell2mat(arrayfun(@(x) (cdf('norm',iPy,x,1) - [0 cdf('norm',iPy(2:length(iPy))-ydel,x,1)]),iPx(1:ceil(length(iPx)/2)),'UniformOutput',false));
-Pxy2 = cell2mat(arrayfun(@(x) (abs(1 - cdf('norm',iPy,x,1) - [1 - cdf('norm',[iPy(1:length(iPy) - 1)+ydel],x,1) 0])),iPx(ceil(length(iPx)/2) + 1:length(iPx)),'UniformOutput',false));
-Pxy = cat(1,Pxy1,Pxy2);
+iPy = [floor(ymin + ydel - 4*cvar):ydel:ceil(ymax + 4*cvar)];
+Pxy = cell2mat(arrayfun(@(x) (cdf('norm',iPy,x,1) - [0 cdf('norm',iPy(2:length(iPy))-ydel,x,1)]),iPx,'UniformOutput',false));
+ysig = size(Pxy,2);
+yi = ysig;
 
 %% Call to convexopt.m
 % to compute Aeq,beq,A,b
-[intmats,intmatC1,intmatC2,intmatC3] = convexopt(cvar,xdel,ydel,sdel,hdel,xi,yi,si,hi,Pxy);
+[intmats,intmatC1,intmatC2,intmatC3] = convexopt(ysig,xdel,ydel,sdel,hdel,xi,yi,si,hi,Pxy);
 
 %% definition of dels, ni
 dels = [xdel ydel sdel hdel];
@@ -42,4 +43,7 @@ Aeq = cat(1,intmatC1,intmatC2,intmatC3);
 beq = cat(1,transpose(Ps),[1],zeros(xi*ysig,1));
 options = optimset('Algorithm','interior-point','Display','final-detailed','MaxFunEvals',1000000);
 [lb x0] = deal(zeros(xi*yi*si*hi,1));
-[x fval] = fmincon(@(Q) costfun(Q,dels,mins,maxs),x0,[],[],Aeq,beq,lb,[],@(Q) nonlcon(cvar,Ps,Pxy,Q,dels,ni,intmats) ,options);
+[x fval] = fmincon(@(Q) costfun(Q,dels,mins,maxs),x0,[],[],Aeq,beq,lb,[],@(Q) nonlcon(ysig,Ps,Pxy,Q,dels,ni,intmats) ,options);
+toc; % elapsed time
+figure(); % user alert
+end % main
